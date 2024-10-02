c50d970db1001f8d16c367fc2efe3dd92395e4f0
ahmedazagaz <contactwithazagaz@gmail.com>
2024-10-02 12:17:59 +0000

    task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,25 +2,15 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-    return Promise.allSettled([
-            signUpUser(firstName, lastName),
-            uploadPhoto(fileName)
-        ])
-        .then((result) => {
-            const array = [];
-            result.forEach((index) => {
-                if (index.status === 'fulfilled') {
-                    array.push({
-                        status: index.status,
-                        value: index.value,
-                    });
-                } else {
-                    array.push({
-                        status: index.status,
-                        value: `Error: ${index.reason.message}`,
-                    });
-                }
-            });
-            return array;
-        });
+  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
+  const result = [];
+  return Promise.allSettled(promises).then((results) => {
+    results.map(({ status, value, reason }) => (
+      result.push({
+        status,
+        value: status === 'rejected' ? reason.toString() : value,
+      })
+    ));
+    return result;
+  });
 }
